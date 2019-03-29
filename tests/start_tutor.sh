4ced8c7953e967a41771b97614f5127bb49edfdf
Silvio Tomatis <silviot@gmail.com>
2019-03-29 18:48:38 +0100

    Fix `error: The requests distribution was not found and is required by kubernetes`

--- tests/start_tutor.sh
+++ tests/start_tutor.sh
@@ -4,7 +4,9 @@ TUTOR_DIR="$( cd "$( dirname "$0" )/.." && pwd )"
 set -ex
 
 cd ${TUTOR_DIR}
-python setup.py develop
+# For reasons beyond my understanding the following command fails the first time it's
+# run, but succeeds the second time around. For this reason we repeat it twice
+python setup.py develop || python setup.py develop
 tutor config save --silent --set ACTIVATE_NOTES=true --set ACTIVATE_XQUEUE=true
 time tutor local pullimages
 time tutor local databases
