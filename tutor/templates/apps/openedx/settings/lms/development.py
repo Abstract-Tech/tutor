c4e300ffb96f5473f983d061879ea0504a04c18e
Silvio Tomatis <silviot@gmail.com>
2019-04-23 15:13:56 +0200

    Factor out common parts from {l,c}ms/{production,development}.py

--- tutor/templates/apps/openedx/settings/lms/development.py
+++ tutor/templates/apps/openedx/settings/lms/development.py
@@ -1,27 +1,10 @@
 import os
 from lms.envs.devstack import *
 
-# Load module store settings from config files
-update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
-# Set uploaded media file path
-MEDIA_ROOT = "/openedx/data/uploads/"
-
-# Video settings
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-
-# Change syslog-based loggers which don't work inside docker containers
-LOGGING['handlers']['local'] = {'class': 'logging.NullHandler'}
-LOGGING['handlers']['tracking'] = {
-    'level': 'DEBUG',
-    'class': 'logging.StreamHandler',
-    'formatter': 'standard',
-}
+execfile(os.path.join(os.path.dirname(__file__), '..', 'common.py'), globals())
 
 # Fix media files paths
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
 PROFILE_IMAGE_BACKEND['options']['location'] = os.path.join(MEDIA_ROOT, 'profile-images/')
 
 ORA2_FILEUPLOAD_BACKEND = 'filesystem'
@@ -36,12 +19,5 @@ GRADES_DOWNLOAD = {
     }
 }
 
-LOCALE_PATHS.append('/openedx/locale')
-
 # Setup correct webpack configuration file for development
 WEBPACK_CONFIG_PATH = 'webpack.dev.config.js'
-
-# Create folders if necessary
-for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE, ORA2_FILEUPLOAD_ROOT]:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
--- tutor/templates/apps/openedx/settings/lms/production.py
+++ tutor/templates/apps/openedx/settings/lms/production.py
@@ -1,27 +1,10 @@
 import os
 from lms.envs.production import *
 
-# Load module store settings from config files
-update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
 
-# Set uploaded media file path
-MEDIA_ROOT = "/openedx/data/uploads/"
-
-# Video settings
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-
-# Change syslog-based loggers which don't work inside docker containers
-LOGGING['handlers']['local'] = {'class': 'logging.NullHandler'}
-LOGGING['handlers']['tracking'] = {
-    'level': 'DEBUG',
-    'class': 'logging.StreamHandler',
-    'formatter': 'standard',
-}
+execfile(os.path.join(os.path.dirname(__file__), '..', 'common.py'), globals())
 
 # Fix media files paths
-VIDEO_IMAGE_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
-VIDEO_TRANSCRIPTS_SETTINGS['STORAGE_KWARGS']['location'] = MEDIA_ROOT
 PROFILE_IMAGE_BACKEND['options']['location'] = os.path.join(MEDIA_ROOT, 'profile-images/')
 
 ALLOWED_HOSTS = [
@@ -61,10 +44,3 @@ GRADES_DOWNLOAD = {
         'location': os.path.join(MEDIA_ROOT, 'grades'),
     }
 }
-
-LOCALE_PATHS.append('/openedx/locale')
-
-# Create folders if necessary
-for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE, ORA2_FILEUPLOAD_ROOT]:
-    if not os.path.exists(folder):
-        os.makedirs(folder)
