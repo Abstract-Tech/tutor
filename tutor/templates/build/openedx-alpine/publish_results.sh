024de8412a8183221e4615764307c6050f74e0c4
Silvio Tomatis <silviot@gmail.com>
2019-04-29 16:56:18 +0200

    Fix variable names

--- tutor/templates/build/openedx-alpine/publish_results.sh
+++ tutor/templates/build/openedx-alpine/publish_results.sh
@@ -4,7 +4,7 @@ for dirname in $@; do
     if ([ -f "$dirname" ] || [ -d "$dirname" ]); then
         echo Pushing "$dirname"
         sudo chown $(id -u) -R "$dirname"
-        REMOTE_DIR=${TEST_RESULTS_BASE_DIR}/${Build.Repository.Provider}/${Build.Repository.Name}/${Build.BuildNumber}
+        REMOTE_DIR=${TEST_RESULTS_BASE_DIR}/${BUILD_REPOSITORY_PROVIDER}/${BUILD_REPOSITORY_ID}/${BUILD_BUILDID}
         REMOTE=${TEST_RESULTS_USER}@${TEST_RESULTS_HOST}
         ssh ${REMOTE} mkdir -p ${REMOTE_DIR}
         scp -r "$dirname" ${REMOTE}:${REMOTE_DIR}
