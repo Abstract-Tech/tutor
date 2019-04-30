e47ed8c991ceeea0592f5fca231d8c0bc093f9d4
Silvio Tomatis <silviot@gmail.com>
2019-04-30 17:43:01 +0200

    Print links for easy consumption

--- tutor/templates/build/openedx-alpine/publish_results.sh
+++ tutor/templates/build/openedx-alpine/publish_results.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+REMOTE_DIR=${TEST_RESULTS_BASE_DIR}/${BUILD_REPOSITORY_PROVIDER}/${BUILD_REPOSITORY_ID}/${BUILD_BUILDID}
+REMOTE_URL=https://${TEST_RESULTS_HOST}/${BUILD_REPOSITORY_PROVIDER}/${BUILD_REPOSITORY_ID}/${BUILD_BUILDID}
+echo Pushing results to ${REMOTE_URL}
 for dirname in $@; do
     if ([ -f "$dirname" ] || [ -d "$dirname" ]); then
         echo Pushing "$dirname"
         sudo chown $(id -u) -R "$dirname"
-        REMOTE_DIR=${TEST_RESULTS_BASE_DIR}/${BUILD_REPOSITORY_PROVIDER}/${BUILD_REPOSITORY_ID}/${BUILD_BUILDID}
         REMOTE=${TEST_RESULTS_USER}@${TEST_RESULTS_HOST}
         ssh ${REMOTE} mkdir -p ${REMOTE_DIR}
         scp -r "$dirname" ${REMOTE}:${REMOTE_DIR}
+        echo copied ${REMOTE_URL}/$dirname
     else
         echo "$dirname" not found. Skipping
     fi
